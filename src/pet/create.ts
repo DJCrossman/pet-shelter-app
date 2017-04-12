b5437a61dcfc42342779f09d41d8242b03354acb
DJCrossman <david@crossman.io>
2017-04-11 21:25:25 -0600

    Pet Weather App page to create pet

--- src/pet/create.ts
+++ src/pet/create.ts
@@ -1,9 +1,38 @@
+import {Redirect, Router} from 'aurelia-router';
 import {PetService} from './pet-service';
 import {inject} from 'aurelia-framework';
 
-@inject(PetService)
+declare var $;
+
+@inject(PetService, Router)
 export class Pet {
   public heading = 'Add your pet';
+  public pet: any;
+  public cancel;
+  public save;
 
-  constructor(private api: PetService){ }
+  constructor(private api: PetService, private router: Router){
+    this.pet = {};
+  }
+
+  addMyPet() {
+    if(this.pet.name && this.pet.typeName && this.pet.breedName &&
+      this.pet.location && this.pet.lat && this.pet.long) {
+      this.toggleButtons('loading');
+      this.api.create(this.pet).then(() => {
+        this.toggleButtons('reset');
+        this.router.navigate('');
+      }).catch((err) => {
+        this.toggleButtons('reset');
+        throw err;
+      });
+    } else {
+      console.log('Your pet is not valid!');
+    }
+  }
+
+  toggleButtons(options) {
+    $(this.cancel).toggle();
+    $(this.save).button(options);
+  }
 }
