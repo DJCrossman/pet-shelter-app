ca144f59c14ff5d0b1c5d996f1456cd253ed06e9
DJCrossman <david@crossman.io>
2017-04-12 18:25:32 -0600

    Add loading to the list screen

--- src/pet/details.ts
+++ src/pet/details.ts
@@ -6,19 +6,27 @@ import {inject} from 'aurelia-framework';
 export class Pet {
   private pet: any;
   private isRaining: boolean;
+  private isLoading: boolean = false;
 
   constructor(private petApi: PetService, private forecastApi: ForecastService){ }
 
   activate(params){
+    this.isLoading = true;
       this.petApi.get(params.id).then((pet:any) => {
         this.pet = pet;
         this.forecastApi.get(pet.lat, pet.long).then((forecast:any) => {
+          this.isLoading = false;
           this.isRaining = forecast.currently.icon === 'rain';
+        }).catch(err => {
+          this.isLoading = false;
         });
+      }).catch(err => {
+        this.isLoading = false;
       });
   }
 
   get heading() {
+    if(typeof this.isRaining === 'undefined') return '';
     return this.isRaining ? 'Yes' : 'Nope';
   }
 
--- src/pet/list.ts
+++ src/pet/list.ts
@@ -6,11 +6,16 @@ export class Pet {
   public heading = 'Does my pet need an umbrella?';
   public subheading = 'Select a pet to find out.';
   public pets: any[];
+  private isLoading: boolean = false;
 
   constructor(private api: PetService){ }
 
   created(){
-      this.api.getAll().then((pets:any[]) => this.pets = pets);
+    this.isLoading = true;
+    this.api.getAll().then((pets:any[]) => {
+      this.isLoading = false;
+      this.pets = pets;
+    });
   }
 
   remove(id) {
